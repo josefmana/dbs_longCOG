0a6de862ec254ba331892ad594656a4095f88b28
josefmana <102481740+josefmana@users.noreply.github.com>
2022-04-25 22:23:42 +0200

    Add files via upload

--- dbs_longCOG.R
+++ dbs_longCOG.R
@@ -12,6 +12,7 @@ pkgs <- c(
   "dplyr", # for objects manipulation
   "tidyverse", # for pivot_longer
   "DiagrammeR", # for flowcharts
+  "rsvg", "DiagrammeRsvg", # for saving plots crated in DiagrammeR
   "brms", # for Bayesian model fitting / interface with STAN
   "psych", # for EFA
   "tidybayes", # for posteriors manipulations
@@ -60,6 +61,7 @@ sapply(
 # of dataset as d# (where # goes for a number of the data version)
 d0 <- read.csv( "data/20220423_dbs_longCOG_data.csv" , sep = "," )
 
+
 # ----------- participant inclusion flowchart  -----------
 
 # check that when selecting only rows containing pre-surgery assessment,
@@ -72,110 +74,78 @@ isTRUE(
 ) # TRUE
 
 # create a table summarizing reasons for excluding patients
-t <- as.data.frame( table(d0[ d0$ass_type == "pre" , ]$why_excluded) )
+t <- table(d0[ d0$ass_type == "pre" , ]$why_excluded)
+print(t)
 
-# extract the numbers for each part of the flowchart
-nums <- list(
-  # total number of patients
-  total = sum( t$Freq ),
-  # patients that didn't receive STN-DBS treatment
-  no_stn = list(
-    total = sum(
-      t$Freq[ t$Var1 %in% c("GPi","VIM","duodopa","rejected","suspended")]
-    ),
-    gpi = t$Freq[ t$Var1 == "GPi" ],
-    vim = t$Freq[ t$Var1 == "VIM" ],
-    duodopa = t$Freq[ t$Var1 == "duodopa" ],
-    rejected = t$Freq[ t$Var1 == "rejected" ],
-    suspended = t$Freq[ t$Var1 == "suspended" ]
-  ),
-  # patients treated with STN-DBS
-  stn_dbs = list(
-    total = sum(
-      t$Freq[ t$Var1 %in% c("included","left_stn_only","right_stn_only","any_psycho_missing","only_drs_before_surgery","pre_psycho_missing",
-                            "post_psycho_missing","language") ]
-    ),
-    included = t$Freq[ t$Var1 == "included" ],
-    unilateral = sum( t$Freq[ t$Var1 %in% paste0( c("left","right") , "_stn_only" ) ] ),
-    preop_miss = sum( t$Freq[ t$Var1 %in% c("any_psycho_missing","only_drs_before_surgery","pre_psycho_missing") ] ),
-    follow_miss = t$Freq[ t$Var1 == "post_psycho_missing" ],
-    language = t$Freq[ t$Var1 == "language" ]
-  )
-)
+# using numbers from t create an inclusion/exclusion flowchart
+flowchart <- " digraph {
+  
+  /// define nodes which will include numbers reflecting the inclusion/exclusion process
+  node [ fontname = Calibri, fontsize = 24, shape = box, style = rounded , width = 2.5 , margin= 0.25 , penwidth = 2 ];
+  
+  /// box for all patients, i.e., sum(t) = 200 patients
+  all_pats [ label =<
+  <b>200 consecutive PD patients </b><br/><br/>Local database 2000-2020<br/>General University Hospital<br/>in Prague
+  >];
+         
+  /// box for all STN-DBS patients, i.e., sum(t[c(1,4,5,6,7,8,9,11)])) = 173 patients
+  all_stn [ label =<
+  <b>173 patients </b><br/><br/>implanted with STN-DBS
+  >];
+  
+  /// box for all included patients, i.e., t[4] = 126 patients
+  all_incl [ label =<
+  <b>126 patients </b><br/><br/>followed-up longitudinally
+  >];
+  
+  /// nodes for exluded patients, specifying only these characteristics that will differ from the nodes above
+  node [ fixedsize = T, width = 5.5, height = 2.2 ];
+  
+  /// box for non-STN-DBS patients, i.e., t[c(3,13,2,10,12)] with sum(t[c(3,13,2,10,12)]) = 27 patients
+  excl_nostn [ label =<
+  <b>27 patients excluded due to</b><br align = 'left'/><br align = 'left'/>
+  12 GPi-DBS<br align = 'left'/>
+  4 VIM-DBS<br align = 'left'/>
+  4 duodopa<br align = 'left'/>
+  5 rejected<br align = 'left'/>
+  2 suspended<br align = 'left'/>
+  >];
+  
+  /// box for STN-DBS excluded patients, i.e., t[c(1,7,9, 8, 6,11, 5)] with sum(t[c(1,7,9,8,6,11,5)]) = 57 patients
+  excl_stn [ label =<
+  <b>47 patients excluded due to</b><br align = 'left'/><br align = 'left'/>
+  24 pre-surgery data missing<br align = 'left'/>
+  18 follow-up data missing<br align = 'left'/>
+  3 unilateral STN-DBS<br align = 'left'/>
+  2 not speaking Czech<br align = 'left'/>
+  >];
+  
+  /// dummy nodes for horizontally forking out of the vertical 'inclusion flow' to excluded sides
+  node [ shape = rectangle, width = 0, height = 0, label = '', fill = black ];
+  
+  /// create directed edges in the inclusion (from dummy/fork vertically to inclusion boxes)
+  /// and the exlusion (from forks horizontally to exclusion boxes) parts of the flowchart
+  /// first make the arrows bigger
+  edge [ arrowsize = 2.5, penwidth = 2.5 ]
+  
+  /// next specifiy paths
+  fork1 -> all_stn; fork2 -> all_incl;
+  
+  /// for the horizontal paths use 'rank = same' to ensure they nodes are level
+  { rank = same ; fork1 -> excl_nostn }
+  { rank = same ; fork2 -> excl_stn }
+  
+  /// create non-directed edges from the inclusion boxes to dummy/fork boxes (vertically)
+  edge [ dir = none, penwidth = 2.5 ]
+  all_pats -> fork1; all_stn -> fork2;
 
-# create a flowchart in DiagrammeR
-grViz(
-  " digraph {
-  
-  # node definitions with substituted label text
-  node [fontname = Calibri, shape = box, style = rounded , width = 2.5]
-  
-  1 [label =
-      <
-      <br/><b>200 consecutive PD patients </b><br/><br/>
-      Local database 2000-2020<br/>
-      General University Hospital<br/>
-      in Prague<br/>
-      >
-    ]
-  
-  2 [label =
-      <
-      <br/><b>173 patients </b><br/><br/>
-      implanted with STN-DBS<br/>
-      >
-    ]
-  
-  3 [label =
-      <
-      <br/><b>126 patients </b><br/><br/>
-      followed-up longitudinally<br/>
-      >
-    ]
-  
-  # nodes for exluded patients
-  node [fontname = Calibri, shape = box, style = rounded, width = 3.2]
-  
-  m1 [label = 
-        <
-        <br align = 'left'/>
-        <b>27 patients excluded due to:</b><br align = 'left'/><br align = 'left'/>
-            12 GPi-DBS<br align = 'left'/> 
-            4 VIM-DBS<br align = 'left'/> 
-            4 duodopa<br align = 'left'/> 
-            5 rejected<br align = 'left'/> 
-            2 suspended<br align = 'left'/>
-        >
-      ]
-
-  m2 [label =
-        <
-        <br align = 'left'/>
-        <b>47 patients excluded due to:</b><br align = 'left'/><br align = 'left'/>
-            24 pre-surgery data missing<br align = 'left'/>
-            18 follow-up data missing<br align = 'left'/>
-            3 unilateral STN-DBS<br align = 'left'/>
-            2 not speaking Czech<br align = 'left'/>
-        >
-     ]
-  
-  # dummy nodes
-  node [ shape = rectangle, width = 0, height = 0, label = '' , fill = black]
-  
-  # edge definitions
-  p1 -> 2; p2 -> 3;
-  {rank = same; p1 -> m1}
-  {rank = same; p2 -> m2}
-  
-  edge [dir = none]
-  1 -> p1; 2 -> p2;
-
-  # seperate nodes in the same rank (in inches)
+  /// finally seperate dummy/fork nodes from exclusion boxes by some reasonable distance (in inches)
   nodesep = 1
-  
-  }
-  "
-)
+  }" 
+
+# save the flowchart as png
+grViz(flowchart) %>% export_svg %>% charToRaw %>% rsvg_png("figures/Fig.1 inclusion-exclusion flowchart.png")
+
 
 # ----------- directed acyclic graph -----------
 
