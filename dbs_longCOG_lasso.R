e3de94331d64674d65b64ca8830571750e9087b4
josefmana <josef.mana@protonmail.com>
2022-11-17 22:36:01 +0100

    tabs s2 and s3 repaired

--- dbs_longCOG_lasso.R
+++ dbs_longCOG_lasso.R
@@ -418,11 +418,10 @@ t <- lapply( names(post), function(i) post[[i]] %>%
              ) %>% `names<-`( c("S3","S2") )
 
 # save the tables as .csv
-for ( i in names(t) ) write.table( t[[i]], sep = ",", row.names = F, na = "",
+for ( i in names(t) ) write.table( t[[i]], sep = ",", row.names = F, na = "", quote = F,
                                    file = paste0("tables/Tab ",i," summary of posteriors (cognitive ",
                                                  ifelse(i=="S3","functions","tests"),").csv" )
                                    )
-
 
 
 # ---- stats for in-text reporting ----
