0effe8f20a95749ff8f2f1d1ea8720cf6e7294b1
josefmana <josef.mana@protonmail.com>
2023-04-30 01:53:22 +0200

    power simulations ...

--- dbs_longCOG_optbias.R
+++ dbs_longCOG_optbias.R
@@ -1,9 +1,14 @@
 # A neat small script that checks for overoptimistic bias in the best case scenario for choosing significant predictors of an
-# outcome from ten independent predictors via two-stage (same data) procedure that first pre-selects potential predictors via
-# simple correlations and then estimates effect sizes via multiple regressions.
+# outcome from a number of predictors via two-stage procedure that first pre-selects potential predictors via simple correlations
+# and then estimates effect sizes via multiple regressions compared to vanilla multiple regression and Bayesian Lasso.
 
 # list packages to be used
-pkgs <- c("rstudioapi", "dplyr", "tidyverse", "MASS", "brms", "bayestestR" )
+pkgs <- c( "rstudioapi", # setting working directory via RStudio API
+           "tidyverse", "dplyr", "reshape2", # data wrangling
+           "MASS", # multivariate normal distribution
+           "brms", "bayestestR", # Bayesian model fitting and summaries
+           "ggplot2" # plotting
+           )
 
 # load or install each of the packages as needed
 for (i in pkgs) {
@@ -18,38 +23,44 @@ setwd( dirname(rstudioapi::getSourceEditorContext()$path) )
 # prints TRUE and creates the folder if it was not present, prints NULL if the folder was already present.
 sapply( c("models", "figures", "tables", "sessions"), function(i) if( !dir.exists(i) ) dir.create(i) )
 
+# set ggplot theme
+theme_set( theme_classic(base_size = 14) )
+
+# formats in which figures are to be saved
+forms <- c(".jpg",".png",".tiff")
+
 
 # ---- prepare data-generating function ----
 
-# prepare a function with varying number of subjects, number of predictors, varying pre-selection threshold and effect size
+# prepare a function with varying number of subjects, number of predictors, varying pre-selection threshold and effect sizes
 # N = number of subjects
 # n_prds = number of independent predictors (integer)
 # eff = list of effect sizes (vector)
-# cor = covariance (identical with correlation in case of standard normals) matrix
-# thres = p-value used a threshold for pre-selection (numeric)
-# meth = either two-step procedure ("two-step"), multiple regression ("multi") or Bayesian lasso ("lasso")
-sim <- function( N = 126, n_prds = 10, eff = rep(0,10), cor = diag( rep(1,10) ), thres = .1, meth = "two-step" ) {
+# cor = covariance matrix (identical with correlation in case of standard normals)
+# thres = p-value used as a threshold for pre-selection (numeric)
+# meth = either two-step procedure ("two-step"), multiple regression ("multi") or Bayesian Lasso ("lasso")
+sim <- function( N = 126, n_prds = 7, eff = rep(0,7), cor = diag( rep(1,7) ), thres = .1, meth = "two-step" ) {
   
-  # generate data and pre-select variables
-  t <- mvrnorm( N, rep(0,n_prds), cor ) # simulate "n_prds" preedictors from multivariate standard normal distribution
+  # simulate "n_prds" pre dictors from multivariate standard normal distribution
+  t <- mvrnorm( N, rep(0,n_prds), cor )
   
   # generate the outcome
-  if ( length(eff) == 1 ) o <- rnorm( N, sum(t[ , 1 ] * eff) )
-  else o <- rnorm( N, rowSums(t[ , 1:length(eff) ] * eff) )
+  if ( length(eff) == 1 ) o <- rnorm( N, sum(t[ , 1 ] * eff) ) # exactly one non-zero effect
+  else o <- rnorm( N, rowSums(t[ , 1:length(eff) ] * eff) ) # none or more than one non-zero effects
   
   # pre-select predictors
-  sel <- sapply( 1:n_prds, function(i) ifelse( cor.test( t[,i], o )$p.value < thres, 1, 0 ) ) # pre-select variables
+  sel <- sapply( 1:n_prds, function(i) ifelse( cor.test( t[,i], o )$p.value < thres, 1, 0 ) )
   
   # compute the final multiple linear regression
   if( meth == "two-step" ) {
     
-    # if the two-step procedure was selected, used only the preselected variables
+    # if the two-step procedure was selected, used only the pre-selected variables
     if( sum(sel) == 0 ) reg <- lm( o ~ 1 ) # intercept only if no variable was selected
     else reg <- lm( o ~ t[ , which(sel == 1) ] )
     
-    # extract final p-values
-    if ( sum(sel) == 0 ) p <- NULL
-    else p <- as.vector( summary(reg)$coefficients[ 2:(sum(sel)+1), 4 ] ) # raw p-values
+    # extract p-values
+    if ( sum(sel) == 0 ) p <- NULL # there ain't no p-value if no variable was pre-selected
+    else p <- as.vector( summary(reg)$coefficients[ 2:(sum(sel)+1), 4 ] )
     
   } else if( meth == "multi" ) {
     
@@ -59,7 +70,7 @@ sim <- function( N = 126, n_prds = 10, eff = rep(0,10), cor = diag( rep(1,10) ),
     
   } else if ( meth == "lasso" ) {
     
-    # finally a Bayesian lasso
+    # finally a Bayesian Lasso
     reg <- brm( as.formula( paste0( "o ~ ", paste( paste0("t",1:n_prds), collapse = " + " ) ) ),
                 prior = prior( lasso(1), class = b ),
                 data =  cbind.data.frame(o, t) %>% `colnames<-`( c("o", paste0("t",1:n_prds) ) )
@@ -71,103 +82,176 @@ sim <- function( N = 126, n_prds = 10, eff = rep(0,10), cor = diag( rep(1,10) ),
   }
   
   # sum-up the results and print them
-  res <- rep(1, n_prds) # prepare ones (for those that were not tested, techniqually p = 1, easier to work with compare to NAs)
+  res <- rep(1, n_prds) # prepare ones (for those that were not tested, technically p = 1, easier to work with than NAs)
   if( meth == "two-step" ) res[ which(sel == 1) ] <- p
   else res <- p # add p-value
   return(res) # return the results
   
 }
 
-# prepare two types of covariance matrixes
-nocov_fun <- function(K = 10) return( diag( rep(1,K) ) ) # a function for no correlation an varying number of measures
-nocov <- nocov_fun(K = 27) # no covariance for K = 27 test measures (number of predictors in Kim et al., 2014)
 
-# putative covariation (correlation in this case) structure of predictors in Kim et al. (2014)
-# based on grouping of tests to common clusters with (mostly) ~ 50 % shared variability (i.e., r = .7, r2 = .49) 
+# ---- set-up covariance matrixes ----
+
+# first prepare a function for no-covariance cases
+nocov_fun <- function(K = 7) return( diag( rep(1,K) ) ) # a function for no correlation with varying number of measures
+nocov <- nocov_fun(K = 23) # no covariance for K = 23 test measures (as many as we use in the current study)
+
+# putative covariation (correlation in this case) structure of predictors in our study
+# based on grouping of tests to common clusters with ~50 % shared variability (i.e., r = .7, R2 = .49) 
 yocov <- matrix( c(
-  1,rep(.7,2),rep(0,24), .7,1,.7,rep(0,24), rep(.7,2),1,rep(0,24), # Trail Making Test
-  rep(0,3),1,rep(0,23), # BNT
-  rep(0,4),1,rep(.7,2),rep(0,20), rep(0,4),.7,1,.7,rep(0,20), rep(0,4),rep(.7,2),1,rep(0,20), # verbal memory
-  rep(0,7),1,.7,rep(0,18), rep(0,7),.7,1,rep(0,18), # nonverbal memory
-  rep(0,9),1,rep(0,17), # RCFT
-  rep(0,10),1,.7,rep(.6,2),rep(.5,2),rep(0,11), rep(0,10),.7,1,rep(.6,2),rep(.5,2),rep(0,11), # Stroop color
-  rep(0,10),rep(.6,2),1,.7,rep(.6,2),rep(0,11), rep(0,10),rep(.6,2),.7,1,rep(.6,2),rep(0,11), # Stroop words
-  rep(0,10),rep(.5,2),rep(.6,2),1,.7,rep(0,11), rep(0,10),rep(.5,2),rep(.6,2),.7,1,rep(0,11), # Stroop color-word
-  rep(0,16),1,.7,rep(0,9), rep(0,16),.7,1,rep(0,9), # verbl fluency
-  rep(0,18),1,rep(0,8), # BDI
-  rep(0,19),1,rep(.7,2),rep(0,5), rep(0,19),.7,1,.7,rep(0,5), rep(0,19),rep(.7,2),1,rep(0,5), # age and duration
-  rep(0,22),1,rep(0,4), # education
-  rep(0,23),1,rep(.7,2),0, rep(0,23),.7,1,.7,0, rep(0,23),rep(.7,2),1,0, # MDS-UPDRS III
-  rep(0,26),1 # LEDD
-), ncol = 27, byrow = T )
+  1, .7, rep(0,21), .7, 1, rep(0,21), # State-Trait Anxiety Inventory
+  rep(0,2), 1, .7, rep(0,19), rep(0,2), .7, 1, rep(0,19), # Trail Making Test
+  rep(0,4), 1, rep(.7,2), rep(0,16), rep(0,4), .7, 1, .7, rep(0,16), rep(0,4), rep(.7,2), 1, rep(0,16), # Digit Span
+  rep(0,7), 1, .7, rep(0,14), rep(0,7), .7, 1, rep(0,14), # Spatial Span
+  rep(0,9), 1, rep(0,13), # Tower of London
+  rep(0,10), 1, rep(.7,2), rep(0,10), rep(0,10), .7, 1, .7, rep(0,10), rep(0,10), rep(.7,2), 1, rep(0,10), # Stroop test
+  rep(0,13), 1, .7, rep(0,8), rep(0,13), .7, 1, rep(0,8), # verbal fluency
+  rep(0,15), 1, rep(0,7), # Similarities (abstraction)
+  rep(0,16), 1, rep(.7,4), rep(0,2), rep(0,16), .7, 1, rep(.7,3), rep(0,2), rep(0,16), rep(.7,2), 1, rep(.7,2), rep(0,2), rep(0,16), rep(.7,3), 1, .7, rep(0,2), rep(0,16), rep(.7,4), 1, rep(0,2), # RAVLT 
+  rep(0,21), 1, .7, rep(0,21), .7, 1 # Family Pictures test
+), ncol = 23, byrow = T )
+
+# plot the correlation matrix
+melt(yocov) %>%
+  rename( "Pearson's r" = "value" ) %>%
+  ggplot( aes(x = Var1, y = rev(Var2), fill = `Pearson's r` ) ) +
+  geom_tile( color = "grey" ) +
+  scale_fill_gradient2( low = "white", high = "navy", limit = c(0,1), midpoint = .33 ) +
+  labs( x = "Row variable", y = "Column variable" ) +
+  scale_x_continuous( breaks = 1:23, labels = 1:23 ) +
+  scale_y_continuous( breaks = 1:23, labels = 23:1 ) +
+  guides( fill = guide_colourbar( barwidth = 1, barheight = 18 ) ) +
+  coord_fixed()
+
+# save it
+for ( i in forms ) ggsave( paste0("figures/Fig S1 simulation correlation matrix",i ), dpi = 300, width = 13.7, height = 7.79 )
 
 
 # ---- simulating the null ----
 
-# prepare a table that shows how false positive error rates depend on the threshold for case with N = 103 (as in Kim et al., 2014)
-# and K = 10 tests
-d0 <-list()
+# prepare a table that shows how false positive error rates depend on the threshold for case with N = 126 (the sample size of our
+# study) and K = 23 tests
+# first read d0 if there is already something computed
+if ( file.exists("models/sims.rds") ) for ( i in names(readRDS("models/sims.RDS")) ) assign( i, readRDS("models/sims.RDS")[[i]] )
 
-# loop through covariance structures and methods of estimation
-for ( i in c("nocov","yocov") ) {
-  for ( j in c("two-step","multi","lasso") ) {
-    
-    d0[[i]][[j]] <- sapply( 1:1e2, function(k)
-      sim( N = 103, n_prds = 27, eff = rep(0,27), cor = get(i), thres = .2, meth = j )
-      ) %>% t() %>% as.data.frame() # tidy up
-    
-  }
-}
-
-# summarize false negatives
-fn0 <- sapply( names(d0), function(i) sapply( names(d0[[i]]), function(j) ifelse( d0[[i]][[j]] < .05, 1, 0 ) %>% rowSums() %>% table() ) )
-
-# the results seem to be almost invariant to threshold used, overall, in ca 2/5 of cases there is at least one false positive
-
-# Under the idealized conditions of this simulation, there is about 11 % chance of getting Kim et al.'s (2014) results of
-# identifying (exactly) three significant predictors even when the null is true
-
-
-# ---- simulating effects ----
-
-# start with one to five medium-size predictors (r = .3) in two settings: a lot (K = 27) and medium amount (K = 10) tests
-d1 <- list()
-
-# loop through the posibilities
-for ( i in c("lot","med") ) {
+# if it ain't there, start from a scratch
+if ( !exists("d0") ) {
   
-  d1[[i]] <- list()
-  K <- case_when( i == "lot" ~ 27, i == "med" ~ 10 ) # number of potential predictors
+  d0 <- list() # prepare a list
   
-  # loop through 1-5 to-be true/false positives
-  for ( j in 1:5 ) {
-    
-    d1[[i]][[j]] <- list()
-    
-    # loop through estimation methods
-    for ( k in c("two-step","multi","lasso") ) {
+  for ( i in c("nocov","yocov") ) {
+    for ( j in c("two-step","multi","lasso") ) {
       
-      d1[[i]][[j]][[k]] <- sapply( 1:1e2, function(x)
-        sim( N = 103, n_prds = K, eff = rep(0.3,j), cor = nocov_fun(K=K), thres = .2, meth = k )
-      ) %>% t() %>% as.data.frame() # tidy up
+      # simulate the data
+      d0[[i]][[j]] <- sapply( 1:1e2, function(k) sim( N = 126, n_prds = 23, eff = rep(0,23), cor = get(i), thres = .2, meth = j ) ) %>% t() %>% as.data.frame()
+      
+      # save after each iteration
+      saveRDS( list( d0 = d0 ), "models/sims.rds" )
+      print( paste0(i, " null, via ", j, " method fitted" ) )
       
     }
   }
 }
 
-# finish by simulating two kinds of effects with covaried predictors where we assume total medium effect of variables that
-# represent TMT (1:3), Stroop colors (11:12) and LEDD (27) from Kim et al. (2014)
-# (i) each variable has a proportinal weight with all clusters linking to 0.3
-# (ii) exactly one variable from each cluster has the total 0.3 weight
-d2 <- list()
+# summarize false negatives
+#fn0 <- sapply( names(d0), function(i) sapply( names(d0[[i]]), function(j) ifelse( d0[[i]][[j]] < .05, 1, 0 ) %>% rowSums() %>% table() ) )
 
-#
-for ( i in c("two-step","multi","lasso") ) {
+# the results seem to be almost invariant to threshold used, overall, in ca 2/5 of cases there is at least one false positive
+
+
+# ---- simulating effects ----
+
+# as the code is fitting hundreds of Stan models, it crashes from time to time, so we will put in place some insurances
+
+# fit one to five medium-size predictors (r = .3) in two settings: a lot (K = 23) and medium amount (K = 10) tests
+# if it there are no simulations of this type (see l. 135), start from a scratch
+if ( !exists("d1") ) {
   
-  d2[[i]]$cluster <- sapply( 1:1e2, function(j) sim( N = 103, n_prds = 27, eff = c( rep(.1,3),rep(0,7),rep(.15,2),rep(0,14),.3 ), cor = yocov, thres = .2, meth = i ) )  %>% t() %>% as.data.frame()
-  d2[[i]]$test <- sapply( 1:1e2, function(j) sim( N = 103, n_prds = 27, eff = c( .3,rep(0,9),.3,rep(0,15),.3 ), cor = yocov, thres = .2, meth = i ) ) %>% t() %>% as.data.frame()
+  d1 <- list() # prepare a list
   
+  # loop through the possibilities
+  for ( i in c("lot","med") ) {
+    
+    d1[[i]] <- list()
+    K <- case_when( i == "lot" ~ 23, i == "med" ~ 7 ) # number of potential predictors
+    
+    # loop through 1-5 to-be true/false positives
+    for ( j in 1:5 ) {
+      
+      d1[[i]][[j]] <- list()
+      
+      # loop through estimation methods
+      for ( k in c("lasso","two-step","multi") ) {
+        
+        # simulate the data
+        d1[[i]][[j]][[k]] <- sapply( 1:1e2, function(x) sim( N = 103, n_prds = K, eff = rep(0.3,j), cor = nocov_fun(K=K), thres = .2, meth = k ) ) %>% t() %>% as.data.frame()
+        
+        # save after each iteration
+        saveRDS( list( d0 = d0, d1 = d1 ), "models/sims.rds" )
+        print( paste0(K, " predictors,", j, " true positives fitted via ", k, " method" ) )
+        
+      }
+    }
+  }
+  
+  # otherwise continue from the next data set after the last one with already computed simulations
+} else {
+  
+  # loop through all the levels, this time no need for creating new lists as they already exist (read from "models/sims.RDS")
+  for ( i in c("lot","med") ) {
+    
+    if( !exists( i, where = d1 ) ) d1[[i]] <- list()
+    
+    K <- case_when( i == "lot" ~ 23, i == "med" ~ 10 ) # number of potential predictors
+    l <- length(d1[[i]]) # number of already simulated true positives
+    
+    for ( j in 1:5) {
+      
+      # compute only settings that are not computed yet
+      if ( j > l ) {
+        
+        d1[[i]][[j]] <- list()
+        
+        # loop through estimation methods
+        for ( k in c("lasso","two-step","multi") ) {
+          
+          # simulate the data
+          d1[[i]][[j]][[k]] <- sapply( 1:1e2, function(x) sim( N = 126, n_prds = K, eff = rep(0.3,j), cor = nocov_fun(K=K), thres = .2, meth = k ) ) %>% t() %>% as.data.frame()
+          
+          # save after each iteration
+          saveRDS( list( d0 = d0, d1 = d1 ), "models/sims.rds" )
+          print( paste0(K, " predictors,", j, " true positives fitted via ", k, " method" ) )
+          
+        }
+      }
+    }
+  }
 }
+
+# finish by simulating two kinds of effects with covaried predictors where we assume total medium effect of variables that
+# represent TMT (3:4), and Stroop test (11:13) inspired by Kim et al. (2014)
+# (i) each variable has a proportional weight with all clusters linking to 0.3
+# (ii) exactly one variable from each cluster has the total 0.3 weight
+if ( !exists("d2") ) {
+  
+  d2 <- list() # prepare a list
+  
+  # loop through estimation methods to get
+  for ( i in c("lasso","two-step","multi") ) {
+    
+    # calculate the simulations
+    d2[[i]]$cluster <- sapply( 1:1e2, function(j) sim( N = 126, n_prds = 23, eff = c( rep(0,2), rep(.15,2),rep(0,6),rep(.1,3),rep(0,10) ), cor = yocov, thres = .2, meth = i ) )  %>% t() %>% as.data.frame()
+    d2[[i]]$test <- sapply( 1:1e2, function(j) sim( N = 126, n_prds = 23, eff = c( rep(0,2), .3, rep(0,7),.3,rep(0,12) ), cor = yocov, thres = .2, meth = i ) ) %>% t() %>% as.data.frame()
+    
+  }
+}
+
+# save it all
+saveRDS( list( d0 = d0, d1 = d1, d2 = d2 ), "models/sims.RDS" )
+
+
+# ---- prepare summaries for plotting ----
+
 
 
 
